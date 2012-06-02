bce34e113c6168f04d2f5de31fe5fda94896385c
Orion <orion.engleton@gmail.com>
2012-06-02 18:04:29 -0400

    Understanding * python and modules

--- app.py
+++ app.py
@@ -1,38 +1,9 @@
 import os as FileCheck
+import sys
+sys.path.append('lib')
 
-from bottle import Bottle
-from bottle import (
-    route, error, HTTPError, get, post, request, response, view, url,
-    cookie_is_encoded, abort, debug
-    )
-from bottle import static_file as HTMLfile
+import app_routes
+from app_routes import app
 from bottle import run as RunApp
-
-app = Bottle()
-
-@app.route('/')
-def index():
-  return HTMLfile('index.html', root='public/')
-
-@app.route('/<filename:re:[a-z]+>')
-def index(filename):
-  assert filename.isalpha()
-  try:
-    open('public/' + filename +'.html')
-    return HTMLfile((filename+'.html'), root='public/')
-  except IOError as e:
-    return HTMLfile('404.html', root='public/errors/')
-    
-@app.route('/javascripts/<filename>')
-def serve_js(filename):
-  return HTMLfile(filename, root='public/javascripts/')
-
-@app.route('/stylesheets/<filename>')
-def serve_stylesheets(filename):
-  return HTMLfile(filename, root='public/stylesheets/')
-
-@app.route('/javascripts/errors/<filename>')
-def serve_error_js(filename):
-  return HTMLfile(filename, root='public/javascripts/errors/')
 
 RunApp(app, host='localhost', port=8999, reloader=True)  
---
+++ lib/app_routes.py
@@ -0,0 +1,37 @@
+from bottle import Bottle
+from bottle import (
+    route, error, HTTPError, get, post, request, response, view, url,
+    cookie_is_encoded, abort, debug
+    )
+from bottle import static_file as HTMLfile
+
+app = Bottle()
+
+@app.route('/')
+def index():
+  return HTMLfile('index.html', root='public/')
+
+@get('/about')
+def about():
+	return HTMLfile('about.html', root='public/')
+
+@app.route('/<filename:re:[a-z]+>')
+def index(filename):
+  assert filename.isalpha()
+  try:
+    open('public/' + filename +'.html')
+    return HTMLfile((filename+'.html'), root='public/')
+  except IOError as e:
+    return HTMLfile('404.html', root='public/errors/')
+    
+@app.route('/javascripts/<filename>')
+def serve_js(filename):
+  return HTMLfile(filename, root='public/javascripts/')
+
+@app.route('/stylesheets/<filename>')
+def serve_stylesheets(filename):
+  return HTMLfile(filename, root='public/stylesheets/')
+
+@app.route('/javascripts/errors/<filename>')
+def serve_error_js(filename):
+  return HTMLfile(filename, root='public/javascripts/errors/')
