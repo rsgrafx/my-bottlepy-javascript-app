e6eacb15aec9ea3391206d755fc9624717f1ec26
Orion <orion.engleton@gmail.com>
2012-06-02 23:01:52 -0400

    Removed cached deleted files

--- app.py
+++
@@ -1,9 +0,0 @@
-import os as FileCheck
-import sys
-sys.path.append('lib')
-
-import app_routes
-from app_routes import app
-from bottle import run as RunApp
-
-RunApp(app, host='localhost', port=8999, reloader=True)  
